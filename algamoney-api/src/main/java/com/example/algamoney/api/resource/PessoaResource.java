b4d252fa5cfafd9eef9ef20bc0668f8e988d883e
mathbloise <matheusvieiraweb@gmail.com>
2019-06-09 19:59:20 -0300

    projeto algaworks

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -0,0 +1,63 @@
+package com.example.algamoney.api.resource;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import com.example.algamoney.api.service.PessoaService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private PessoaService pessoaService;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @GetMapping
+    public List<Pessoa> listar() {
+        return pessoaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+        Pessoa pessoa = pessoaRepository.findOne(codigo);
+        return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+        pessoaRepository.delete(codigo);
+    }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+        Pessoa pessoaAtualizada = pessoaService.atualizarPessoa(codigo, pessoa);
+        return ResponseEntity.ok(pessoaAtualizada);
+    }
+}
